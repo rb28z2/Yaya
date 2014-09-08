da16eb7cef5d53ff55ab4033a3dc4bcaa70e88e4
Smoothtalk <avinash1010@gmail.com>
2014-09-07 21:43:10 -0600

    B

--- app/src/main/java/ca/currybox/daru/main.java
+++ app/src/main/java/ca/currybox/daru/main.java
@@ -12,6 +12,7 @@ public class main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Boobies");
     }
 
 
