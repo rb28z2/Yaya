dc2972cec1effbacbbea1434d8dea2f26c2ad53a
shinigamibob <kmarasinghe1@gmail.com>
2014-09-11 12:49:26 -0600

    Added preliminary support for a settings screen. No actual functionality, but it looks nice...

---
+++ app/src/main/java/ca/currybox/daru/SettingsActivity.java
@@ -0,0 +1,105 @@
+package ca.currybox.daru;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by Kanchana on 9/10/2014.
+ */
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Display the fragment as the main content
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            //Load the preferences from the XML resource
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            // Set up a listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences()
+                    .registerOnSharedPreferenceChangeListener(this);
+
+            initSummary();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+
+            // Unregister the listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences()
+                    .unregisterOnSharedPreferenceChangeListener(this);
+        }
+
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            updatePrefsSummary(sharedPreferences, findPreference(key));
+        }
+
+        /**
+         * Update summary
+         *
+         * @param sharedPreferences
+         * @param pref
+         */
+        protected void updatePrefsSummary(SharedPreferences sharedPreferences, Preference pref) {
+
+            if (pref == null)
+                return;
+
+            if (pref instanceof EditTextPreference) {
+                // EditPreference
+                EditTextPreference editTextPref = (EditTextPreference) pref;
+                editTextPref.setSummary(editTextPref.getText());
+            }
+        }
+
+        /*
+        Initialize summary for all items
+         */
+        protected void initSummary() {
+            for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+                initPrefsSummary(getPreferenceManager().getSharedPreferences(),
+                        getPreferenceScreen().getPreference(i));
+            }
+        }
+
+        /*
+        Initialize summary for single elements
+         */
+        protected void initPrefsSummary(SharedPreferences sharedPreferences, Preference p) {
+            if (p instanceof PreferenceCategory) {
+                PreferenceCategory pCat = (PreferenceCategory) p;
+                for (int i = 0; i < pCat.getPreferenceCount(); i++) {
+                    initPrefsSummary(sharedPreferences, pCat.getPreference(i));
+                }
+            } else {
+                updatePrefsSummary(sharedPreferences, p);
+            }
+        }
+
+    }
+
+}
+
+
