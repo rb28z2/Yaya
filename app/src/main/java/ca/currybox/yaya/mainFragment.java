cf53888820da0b12c24a6205bcc52e74fbe883eb
write_only_memory <kmarasinghe1@gmail.com>
2015-01-24 20:21:27 -0700

    Pull down to refresh implemented and is currently awesome.

--- app/src/main/java/ca/currybox/yaya/mainFragment.java
+++ app/src/main/java/ca/currybox/yaya/mainFragment.java
@@ -1,5 +1,6 @@
 package ca.currybox.yaya;
 
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -24,21 +25,16 @@ import java.util.List;
 /**
  * Created by Kanchana on 12/19/2014.
  */
-public class mainFragment extends Fragment implements View.OnClickListener {
+public class mainFragment extends Fragment{
 
-    private TextView status;
-    private List<Anime> animeList = null;
-    ListView listview;
-    ListViewAdapter adapter;
+
 
 
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.main_layout, container, false);
 
-        Button retrieve = (Button) view.findViewById(R.id.retrieve_button);
-        retrieve.setOnClickListener(this);
-
+        super.getFragmentManager().beginTransaction().replace(R.id.main_list, new animeListView()).commit();
 
         SharedPreferences firstLaunch = super.getActivity().getSharedPreferences("FirstLaunch", Context.MODE_PRIVATE); //get object for below
         boolean shownPrefs = firstLaunch.getBoolean("HaveShownPrefs", false); //gets the value to check if application has been launched at least once
@@ -51,14 +47,7 @@ public class mainFragment extends Fragment implements View.OnClickListener {
         }
 
         //Follow block checks if user xml file exists. If it does, it populates the listview. Otherwise, do nothing
-        File user = new File(super.getActivity().getFilesDir().toString() + "/user.xml");
 
-
-        if (user.exists()) {
-            new PopulateList().execute();
-        } else {
-            //not found conditions here
-        }
 
         Log.i("Fragment", "We're here");
 
@@ -66,77 +55,5 @@ public class mainFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
-    @Override
-    public void onClick(View v)
-    {
-        switch (v.getId())
-        {
-            case R.id.retrieve_button:
-                retrieveList();
-        }
-    }
 
-    public void retrieveList() {
-        status = (TextView) super.getActivity().findViewById(R.id.status); //the status info box
-        DownloadUser task = new DownloadUser();
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(super.getActivity().getApplicationContext()); //preferences object
-        String username = prefs.getString("pref_mal_username", ""); //gets the username from preferences
-        String url = "http://myanimelist.net/malappinfo.php?u=" + username + "&status=all&type=anime"; //creates a valid url
-        status.setText("Fetching data...");
-
-        task.execute(new String[]{url}); //get data asynchronously
-
-
-    }
-
-    private class PopulateList extends AsyncTask<Void, Void, Void> {
-
-        protected Void doInBackground(Void... params) {
-            XMLParser parser = new XMLParser();
-            String xml = parser.read("user.xml", getActivity());
-            Document doc = parser.getDomElement(xml);
-
-            animeList = new animeList().getList(doc);
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-
-            listview = (ListView) mainFragment.super.getActivity().findViewById(R.id.shows);
-
-
-            adapter = new ListViewAdapter(mainFragment.super.getActivity(), animeList);
-
-            listview.setAdapter(adapter);
-
-            adapter.filter(1); //type 1 is currently watching
-            adapter.sortByUpdated(); //sorts the list by last updated
-            status = (TextView) mainFragment.super.getActivity().findViewById(R.id.status);
-
-            status.setText("Waaaai~~"); //te-he~
-        }
-
-    }
-
-
-    private class DownloadUser extends AsyncTask<String, Void, Void> {
-
-        protected Void doInBackground(String... urls) {
-
-            for (String url : urls) {
-
-                XMLParser parser = new XMLParser();
-                parser.getXmlFromUrl(url);
-                parser.write("user.xml", mainFragment.super.getActivity());
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-
-            new PopulateList().execute();
-        }
-    }
 }
