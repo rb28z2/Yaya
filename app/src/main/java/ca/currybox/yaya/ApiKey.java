ff823b959d4866c3b8bcf4747ff12250c1fd2d3e
write-only-memory <kmarasinghe1@gmail.com>
2015-09-10 16:05:22 -0600

    Updated project for latest Android Studio (1.3)

---
+++ app/src/main/java/ca/currybox/yaya/ApiKey.java
@@ -0,0 +1,11 @@
+package ca.currybox.yaya;
+
+/**
+ * Created by write_only_memory on 11/12/2014.
+ */
+public class ApiKey {
+
+    public String getKey() {
+        return "your api key here";
+    }
+}
