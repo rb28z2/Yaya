7f26f59de9697c0cda9beadb549f5808ab77255c
write-only-memory <kmarasinghe1@gmail.com>
2015-09-10 19:20:12 -0600

    Revert "Ignoring stuff"
    This reverts commit 6a5440d4b5f01182e95f78ec776c149a71b40939.
    
    	new file:   app/src/main/java/ca/currybox/yaya/ApiKey.java

---
+++ app/src/main/java/ca/currybox/yaya/ApiKey.java
@@ -0,0 +1,11 @@
+package ca.currybox.yaya;
+
+/**
+ * Created by write_only_memory on 11/12/2014.
+ */
+public class ApiKey {
+
+    public String getKey() {
+        return "your api key here"; //get your api key from mal and enter it here. you need your own key if you build/fork the app
+    }
+}
