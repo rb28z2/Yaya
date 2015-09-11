c869deb7d1279e507cba00a2408d5817e18e3aeb
write-only-memory <kmarasinghe1@gmail.com>
2015-09-10 18:44:42 -0600

    Some comments and stuff. Updated gitignore.

--- app/src/main/java/ca/currybox/yaya/ApiKey.java
+++ app/src/main/java/ca/currybox/yaya/ApiKey.java
@@ -6,6 +6,6 @@ package ca.currybox.yaya;
 public class ApiKey {
 
     public String getKey() {
-        return "your api key here"; //get your api key from mal and enter it here. you need your own key if you build/fork the app
+        return "your api key here"; //get your api key from mal and enter it here. you need your own key if you build/fork the app. test
     }
 }
