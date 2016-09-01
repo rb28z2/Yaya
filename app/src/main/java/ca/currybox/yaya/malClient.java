cf3d774cc0d65d6686d9e0267bbac16632e4330c
Avinash <avinash1010@gmail.com>
2016-08-31 21:54:15 -0600

    Praise Harambe, he solves all the problems in the world.

---
+++ app/src/main/java/ca/currybox/yaya/malClient.java
@@ -0,0 +1,46 @@
+package ca.currybox.yaya;
+
+import android.util.Log;
+
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by Avinash on 2016-08-30.
+ */
+
+public class malClient extends AsyncHttpClient{
+    //@TODO Figure out what user-agent the client is sending
+    private static String BASE_URL = "myanimelist.net/api/anime/search.xml?q=";
+    //private static String UA_URL = "http://headers.cloxy.net/request.php";
+    private static String USERNAME;
+    private static String PASSWORD;
+
+    private static AsyncHttpClient malClient = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        malClient.setUserAgent(new ApiKey().getKey());
+        malClient.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void getFile(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        malClient.setUserAgent(new ApiKey().getKey());
+        malClient.get(url, params, responseHandler);
+    }
+
+    public void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        malClient.setUserAgent(new ApiKey().getKey());
+        malClient.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return "https://" + USERNAME + ":" + PASSWORD + "@" + BASE_URL + relativeUrl;
+        //return BASE_URL + relativeUrl;
+    }
+
+    public void setCreds(String userName, String password){
+        USERNAME = userName;
+        PASSWORD = password;
+    }
+}
